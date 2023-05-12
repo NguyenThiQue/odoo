61bc663a2048cd4e79df79b0bcdf9ec6f0aea4e4
NguyenThiQue <quenguyenbt@gmail.com>
2023-05-12 16:47:04 +0700

    add demo

---
+++ tutorial_category/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "tutorial_category",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Services/Game',
+    'version': '16.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['tutorial'],
+
+    # always loaded
+    'data': [
+        "security/ir.model.access.csv",
+        "views/tag_view.xml",
+        "views/game_view.xml",
+        "views/menu_view.xml",
+        "views/game_list_template.xml",
+    ],
+    'application':False,
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
